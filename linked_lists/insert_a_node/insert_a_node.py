1fa85a760dc671f909521b45c0f60595f01846d5
Zhaoyu Zhang <dreamibor@users.noreply.github.com>
2018-11-02 22:57:39 +0000

    add linked lists

---
+++ linked_lists/insert_a_node/insert_a_node.py
@@ -0,0 +1,87 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the insertNodeAtPosition function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def insertNodeAtPosition(head, data, position):
+    if position == 0:
+        current_node = SinglyLinkedListNode(data, head)
+        return current_node
+    else:
+        prev = head
+        curr = head.next
+        count = 1
+        while count < position:
+            prev = curr
+            curr = curr.next
+            count += 1
+        new_node = SinglyLinkedListNode(data)
+        prev.next = new_node
+        new_node.next = curr
+        return head
+# How to optimise the code, other edge use cases?
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    llist_count = int(input())
+
+    llist = SinglyLinkedList()
+
+    for _ in range(llist_count):
+        llist_item = int(input())
+        llist.insert_node(llist_item)
+
+    data = int(input())
+
+    position = int(input())
+
+    llist_head = insertNodeAtPosition(llist.head, data, position)
+
+    print_singly_linked_list(llist_head, ' ', fptr)
+    fptr.write('\n')
+
+    fptr.close()
